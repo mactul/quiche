8c3e10227f827ba848c23ed52e813502c79a3091
mactul <mactulgames@gmail.com>
2025-08-11 10:23:08 +0200

    Fix ack frequency wire_len()

--- quiche/src/tests.rs
+++ quiche/src/tests.rs
@@ -50,6 +50,7 @@ fn transport_params() {
         retry_source_connection_id: Some(b"retry".to_vec().into()),
         max_datagram_frame_size: Some(32),
         unknown_params: Default::default(),
+        min_ack_delay: None,
     };
 
     let mut raw_params = [42; 256];
@@ -80,6 +81,7 @@ fn transport_params() {
         retry_source_connection_id: None,
         max_datagram_frame_size: Some(32),
         unknown_params: Default::default(),
+        min_ack_delay: None,
     };
 
     let mut raw_params = [42; 256];
