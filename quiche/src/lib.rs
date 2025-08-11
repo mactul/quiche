c654574315f807725eb70563dfe5f6a63a946f8a
mactul <mactulgames@gmail.com>
2025-08-11 12:44:51 +0200

    Change min_ack_delay id to fit with the draft

--- quiche/src/lib.rs
+++ quiche/src/lib.rs
@@ -9075,7 +9075,7 @@ impl TransportParams {
                     tp.active_conn_id_limit = limit;
                 },
 
-                0xde1a => {
+                0xff04de1b => {
                     tp.min_ack_delay = Some(val.get_varint()?);
                 },
 
@@ -9269,7 +9269,7 @@ impl TransportParams {
         if let Some(min_ack_delay) = tp.min_ack_delay {
             TransportParams::encode_param(
                 &mut b,
-                0xde1a,
+                0xff04de1b,
                 octets::varint_len(min_ack_delay)
             )?;
             b.put_varint(min_ack_delay)?;
